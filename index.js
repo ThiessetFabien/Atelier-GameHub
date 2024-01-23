5505510b2d60f8e494558e886fa7ad4ef462d412
simonc56 <simchat@gmail.com>
2024-01-23 12:07:44 +0100

    étapes 3, 4 et 5 : factorisation et appel dynamique de header.ejs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const games = require('./games.json');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = 2500;
 
 //moteur de template
 app.set( 'view engine', 'ejs');
@@ -14,7 +14,21 @@ app.set( 'views', './views' );
 app.use(express.static( './public' ));
 
 app.get( '/', (req,res) => {
-  res.render('index.ejs', {games} );
+  res.render('index', {games} );
+});
+
+app.get( '/game/:game', (req,res) => {
+  const game = req.params.game;
+  const test = games.find(foundGame => foundGame.name === game);
+    if (test) {
+        res.render(game, {games, game});
+    } else {
+        res.sendStatus(404);
+    };
+});
+
+app.use((req, res)=>{
+  res.sendStatus(404);
 });
 
 app.listen(PORT, () => {
