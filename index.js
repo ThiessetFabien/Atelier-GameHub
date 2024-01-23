768086b472ee33deb08ff3e0533b44461e8439ec
simonc56 <simchat@gmail.com>
2024-01-23 10:25:29 +0100

    etape 2: intégration index.ejs avec games.json

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // initialisation express
 const express = require( 'express' );
+const games = require('./games.json');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 5000;
 
 //moteur de template
 app.set( 'view engine', 'ejs');
 // définition du dossier contenant les vues
-app.set( 'views', '/views' );
+app.set( 'views', './views' );
 
 app.use(express.static( './public' ));
+
+app.get( '/', (req,res) => {
+  res.render('index.ejs', {games} );
+});
 
 app.listen(PORT, () => {
   console.log( `http://localhost:${PORT}` );
